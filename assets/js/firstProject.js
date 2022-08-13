342e506d32bbe6ec21408aacc263efe29f2313ab
francis62 <franciscomoyano424@gmail.com>
2022-08-13 12:38:42 -0300

    First commmit

---
+++ assets/js/firstProject.js
@@ -0,0 +1,13 @@
+
+
+var navbarHamburger = document.getElementById("navbarHamburger")
+
+  function mostrarMenu(){
+      navbar.style.right = "0";
+  }
+
+  function ocultarMenu(){
+      navbar.style.right = "-200px";
+  }
+
+
---
+++ assets/js/fourthFunction.js
@@ -0,0 +1,34 @@
+/*
+var navbar = document.getElementClassName("navbar-end")
+
+  function showMenu(){
+      navbar.style.right = "0";
+  }
+
+  function hiddenMenu(){
+      navbar.style.right = "-200px";
+  }
+
+
+  $(window).resize(function(){
+    if($(this).width() > 700){
+      window.onscroll = function() {myFunction()};
+  
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+  
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
+  
+    jQuery(document).ready(function(){
+      jQuery("navbar").wrap('<div class="nav-placeholder"></div>');
+      jQuery(".nav-placeholder").height(jQuery("navbar").outerHeight());
+    });
+    }
+  });
+  */
